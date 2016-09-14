1fff18de3da9a2f4668a0acdc712e33eb380ce59
Tal Liron <tal.liron@gmail.com>
2016-09-14 17:17:48 -0500

    Cloudify: fixes to relationship properties

--- src/cloudify/aria_extension_cloudify/v1_0/utils/relationships.py
+++ src/cloudify/aria_extension_cloudify/v1_0/utils/relationships.py
@@ -25,10 +25,6 @@ from aria.presentation import Value
 def get_relationship_inherited_property_definitions(context, presentation):
     properties = get_inherited_property_definitions(context, presentation, 'properties')
     
-    for key in properties.iterkeys():
-        if key != 'connection_type':
-            context.validation.report('relationship type "%s" has unsupported property: %s' % (presentation._fullname, repr(key)), locator=presentation._get_grandchild_locator('properties', key), level=Issue.BETWEEN_FIELDS)
-    
     definition = properties.get('connection_type')
     if definition is not None:
         default = definition.default
@@ -44,9 +40,9 @@ def get_relationship_inherited_property_definitions(context, presentation):
 def get_relationship_assigned_and_defined_property_values(context, presentation):
     values = get_assigned_and_defined_property_values(context, presentation)
 
-    value = values.get('connection_type', Value(str, 'all_to_all'))
-    if value.value not in ('all_to_all', 'all_to_one'):
-        context.validation.report('"connection_type" property is not "all_to_all" or "all_to_one" in relationship in node template "%s": %s' % (presentation._container._fullname, repr(value)), locator=presentation._get_grandchild_locator('properties', 'connection_type'), level=Issue.BETWEEN_FIELDS)
-    values['connection_type'] = value
+    if 'connection_type' in values:
+        value = values['connection_type']
+        if value.value not in ('all_to_all', 'all_to_one'):
+            context.validation.report('"connection_type" property is not "all_to_all" or "all_to_one" in relationship in node template "%s": %s' % (presentation._container._fullname, repr(value)), locator=presentation._get_grandchild_locator('properties', 'connection_type'), level=Issue.BETWEEN_FIELDS)
     
     return values
