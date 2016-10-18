00cb4a5b2959bf7c8be98a0a470beb37382f0649
Tal Liron <tal.liron@gmail.com>
2016-10-18 11:15:24 -0500

    Make Apache license headers consistent

--- src/tests/cloudify/test_data_types.py
+++ src/tests/cloudify/test_data_types.py
@@ -1,3 +1,19 @@
+#
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
 from .suite import (get_node_by_name,
                     ParserTestCase,
                     TempDirectoryTestCase,
@@ -22,7 +38,7 @@ class TestDataTypes(ParserTestCase, TempDirectoryTestCase):
         self.template.data_types_section(
             properties_first='\n        type: unknown-type')
         self.assert_parser_issue_messages(
-            ['"type" refers to an unknown data type in "first": u\'unknown-type\''])
+            ['"type" refers to an unknown data type in "first": \'unknown-type\''])
 
     def test_simple(self):
         self.template.version_section('cloudify_dsl', '1.2')
